680f2af94d7a76080828829d5a650a094b05461d
Octavian Oncescu <octavian.oncescu@gmail.com>
2019-07-08 11:56:56 +0200

    Added main functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,55 @@
 // Copyright 2019 Octavian Oncescu
 
 use hashbrown::HashMap;
-use std::borrow::ToOwned;
+use std::borrow::{Cow, ToOwned};
 use std::hash::Hash;
-use std::marker::PhantomData;
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum EntryForm {
+    /// The map entry is borrowed.
+    Borrowed,
+
+    /// The map entry is owned.
+    Owned,
+}
+
 pub struct CowHashMap<'a, K, V> 
     where K: Hash + Sized + PartialEq + Eq,
-          V: 'a + ToOwned + 'a + Sized,
+          V: ToOwned + ?Sized,
 {
-    inner: HashMap<K, V>,
-    phantom: PhantomData<&'a V>
+    inner: HashMap<K, Cow<'a, V>>
 }
 
 impl<'a, K, V> CowHashMap<'a, K, V> 
     where K: Hash + Sized + PartialEq + Eq,
-          V: 'a + ToOwned + 'a + Sized,
+          V: ToOwned + ?Sized,
 {
     /// Creates a new `CowHashMap`.
     /// 
     /// ```rust
-    /// use cow_hashmap::CowHashMap;
+    /// use hashcow::CowHashMap;
     /// 
-    /// let hm: CowHashMap<String, String> = CowHashMap::new();
+    /// let hm: CowHashMap<&str, String> = CowHashMap::new();
     /// ```
+    #[inline]
     pub fn new() -> Self {
         CowHashMap {
-            inner: HashMap::new(),
-            phantom: PhantomData
+            inner: HashMap::new()
         }
     }
 
     /// Creates a new `CowHashMap` with the specified capacity.
     /// 
     /// ```rust
-    /// use cow_hashmap::CowHashMap;
+    /// use hashcow::CowHashMap;
     /// 
-    /// let hm: CowHashMap<String, String> = CowHashMap::with_capacity(5);
+    /// let hm: CowHashMap<&str, String> = CowHashMap::with_capacity(5);
     /// assert!(hm.capacity() >= 5);
     /// ```
+    #[inline]
     pub fn with_capacity(capacity: usize) -> Self {
         CowHashMap {
-            inner: HashMap::with_capacity(capacity),
-            phantom: PhantomData
+            inner: HashMap::with_capacity(capacity)
         }
     }
 
@@ -52,26 +58,175 @@ impl<'a, K, V> CowHashMap<'a, K, V>
     /// This number is a lower bound; the map might be able to hold more elements, but is guaranteed to be able to hold at least this many elements.
     /// 
     /// ```rust
-    /// use cow_hashmap::CowHashMap;
+    /// use hashcow::CowHashMap;
     /// 
-    /// let hm: CowHashMap<String, String> = CowHashMap::new();
+    /// let hm: CowHashMap<&str, [u8]> = CowHashMap::new();
     /// assert_eq!(hm.capacity(), 0);
     /// ```
+    #[inline]
     pub fn capacity(&self) -> usize {
         self.inner.capacity()
     }
 
     /// Reserves capacity for at least additional more elements to be inserted in the map. 
     /// The collection may reserve more space to avoid frequent reallocations.
+    #[inline]
     pub fn reserve(&mut self, additional: usize) {
         self.inner.reserve(additional);
     }
+
+    /// Inserts a new key/value pair into the map with the value
+    /// being in the owned form.
+    /// 
+    /// This function returns `None` if there was no value previously 
+    /// associated with the given key. If the key is replaced, this
+    /// function returns the previous value. If the previous value
+    /// is borrowed, it will be cloned and then returned.
+    /// 
+    /// ```rust
+    /// use hashcow::CowHashMap;
+    /// 
+    /// let mut hm: CowHashMap<&str, [u8]> = CowHashMap::new();
+    /// hm.insert_owned("key", vec![1, 2, 3]);
+    ///
+    /// assert_eq!(hm.len(), 1);
+    /// ```
+    #[inline]
+    pub fn insert_owned(&mut self, key: K, value: <V as ToOwned>::Owned) -> Option<<V as ToOwned>::Owned> {
+        self.inner.insert(key, Cow::Owned(value)).map(|x| x.into_owned())
+    }
+
+    /// Inserts a new key/value pair in to the map with the value
+    /// being in borrowed form.
+    /// 
+    /// This function returns `None` if there was no value previously 
+    /// associated with the given key. If the key is replaced, this
+    /// function returns the previous value. If the previous value
+    /// is borrowed, it will be cloned and then returned.
+    /// 
+    /// ```rust
+    /// use hashcow::CowHashMap;
+    /// 
+    /// let mut hm: CowHashMap<&str, [u8]> = CowHashMap::new();
+    /// hm.insert_borrowed("key", &[1, 2, 3]);
+    ///
+    /// assert_eq!(hm.len(), 1);
+    /// ```
+    #[inline]
+    pub fn insert_borrowed(&mut self, key: K, value: &'a V) -> Option<<V as ToOwned>::Owned> {
+        self.inner.insert(key, Cow::Borrowed(value)).map(|x| x.into_owned())
+    }
+
+    /// Attempts to retrieve a reference to an item stored in the map.
+    /// 
+    /// ```rust
+    /// use hashcow::CowHashMap;
+    /// 
+    /// let mut hm: CowHashMap<&str, [u8]> = CowHashMap::new();
+    /// hm.insert_borrowed("key1", &[1, 2, 3]);
+    /// hm.insert_owned("key2", vec![4, 5, 6]);
+    ///
+    /// assert_eq!(hm.len(), 2);
+    /// assert_eq!(hm.get(&"key1").unwrap(), &[1, 2, 3]);
+    /// assert_eq!(hm.get(&"key2").unwrap(), &[4, 5, 6]);
+    /// ```
+    #[inline]
+    pub fn get(&self, key: &K) -> Option<&V> {
+        self.inner.get(key).map(|v| v.as_ref())
+    }
+
+    /// Attempts to retrieve a mutable reference to the owned
+    /// form of an item stored in the map. 
+    /// 
+    /// If the stored entry is in the borrowed form, this function
+    /// will clone the underlying data.
+    /// 
+    /// ```rust
+    /// use hashcow::{EntryForm, CowHashMap};
+    /// 
+    /// let mut hm: CowHashMap<&str, [u8]> = CowHashMap::new();
+    /// hm.insert_borrowed("key1", &[1, 2, 3]);
+    /// 
+    /// assert_eq!(hm.entry_form(&"key1").unwrap(), EntryForm::Borrowed);
+    /// 
+    /// {
+    ///     // This will clone the entry stored at this key
+    ///     let entry = hm.get_mut(&"key1").unwrap();
+    ///     assert_eq!(entry, &mut vec![1, 2, 3]);
+    ///     
+    ///     *entry = vec![4, 5, 6];
+    /// }
+    ///
+    /// assert_eq!(hm.entry_form(&"key1").unwrap(), EntryForm::Owned);
+    /// assert_eq!(hm.get(&"key1").unwrap(), &[4, 5, 6]);
+    /// ```
+    #[inline]
+    pub fn get_mut(&mut self, key: &K) -> Option<&mut <V as ToOwned>::Owned> {
+        self.inner.get_mut(key).map(|v| v.to_mut())
+    }
+
+    /// Makes a specific value in the map owned, if it isn't so already.
+    /// 
+    /// This function does not do anything if the value is already in owned
+    /// form.
+    #[inline]
+    pub fn make_owned(&mut self, key: &K) -> Option<&V> {
+        let val = self.inner.get_mut(key)?;
+        
+        match val {
+            Cow::Borrowed(v) => {
+                *val = Cow::Owned(v.to_owned());
+                self.inner.get(key).map(|v| v.as_ref())
+            }
+
+            Cow::Owned(_) => {
+                self.inner.get(key).map(|v| v.as_ref())
+            }
+        }
+    }
+
+    /// Returns the number of elements that are currently in the map.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+
+    /// If an entry with the given key exists, this function
+    /// returns the underlying form in which it is stored in 
+    /// the map.
+    /// 
+    /// Can be either `EntryForm::Borrowed` or `EntryForm::Owned`.
+    #[inline]
+    pub fn entry_form(&self, key: &K) -> Option<EntryForm> {
+        let val = self.inner.get(key)?;
+
+        match val {
+            Cow::Borrowed(_) => Some(EntryForm::Borrowed),
+            Cow::Owned(_) => Some(EntryForm::Owned),
+        }
+    }
+}
+
+impl<'a, K, V> ToOwned for CowHashMap<'a, K, V> 
+    where K: Hash + Sized + PartialEq + Eq,
+          V: ToOwned + ?Sized,
+{
+    type Owned = Self;
+
+    #[inline]
+    fn to_owned(&self) -> Self::Owned {
+        unimplemented!();
+    } 
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*;
+
+    // #[test]
+    // fn it_clones_data_lazily() {
+    //     let hm = CowHashMap::new();
+
+    //     hm.insert("key", )
+    // }
 }
